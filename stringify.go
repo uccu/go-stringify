02e552497d3ff2a66f5996e27f59345b280ad433
uccu <mbmbn123@hotmail.com>
2021-03-09 21:55:35 +0800

    init

--- stringify.go
+++ stringify.go
@@ -1,6 +1,78 @@
 package stringify
 
-func Stringify(i interface{}) string {
+import (
+	"fmt"
+	"reflect"
+	"strconv"
+	"strings"
+)
+
+type String interface {
+	String() string
+}
+
+func ToString(i interface{}, subs ...string) string {
+
+	valueElement := getReflectValue(i)
+	valueTypeKind := valueElement.Type().Kind()
+
+	if e, ok := valueElement.Interface().(String); ok {
+		return e.String()
+	}
+
+	if valueTypeKind == reflect.String {
+		return valueElement.String()
+	}
+
+	if valueTypeKind == reflect.Bool {
+		if valueElement.Bool() == true {
+			return "true"
+		}
+		return "false"
+	}
+
+	if valueTypeKind <= reflect.Int64 {
+		return strconv.FormatInt(valueElement.Int(), 10)
+	}
+
+	if valueTypeKind <= reflect.Uintptr {
+		return strconv.FormatUint(valueElement.Uint(), 10)
+	}
+
+	if valueTypeKind <= reflect.Float64 {
+		if len(subs) > 0 && subs[0] != "" {
+			f, _ := strconv.ParseInt(subs[0], 10, 64)
+			return strconv.FormatFloat(valueElement.Float(), 'f', int(f), 64)
+		}
+		return fmt.Sprintf("%v", valueElement.Float())
+	}
+
+	if valueTypeKind == reflect.Slice {
+
+		if _, ok := valueElement.Interface().([]string); !ok {
+			return ""
+		}
+		sub := ","
+		if len(subs) > 0 && subs[0] != "" {
+			sub = subs[0]
+		}
+		return strings.Join((valueElement.Slice(0, valueElement.Cap()).Interface()).([]string), sub)
+	}
 
 	return ""
+}
+
+func getReflectValue(value interface{}) reflect.Value {
+	if e, ok := value.(reflect.Value); ok {
+		return e
+	} else {
+		valueElement := reflect.ValueOf(value)
+		valueElement = reflect.ValueOf(valueElement.Interface())
+		valueTypeKind := valueElement.Type().Kind()
+		if valueTypeKind == reflect.Ptr {
+			return valueElement.Elem()
+		} else {
+			return valueElement
+		}
+	}
 }
--- stringify_test.go
+++ stringify_test.go
@@ -1,7 +1,224 @@
 package stringify_test
 
-import "testing"
+import (
+	"log"
+	"testing"
 
-func Test(t *testing.T) {
+	. "github.com/uccu/go-stringify"
+)
+
+func TestBool(t *testing.T) {
+	s := ToString(true)
+	if s != "true" {
+		t.Error("err")
+	}
+	log.Println("success", s)
+
+	s = ToString(false)
+	if s != "false" {
+		t.Error("err")
+	}
+	log.Println("success", s)
+}
+
+func TestString(t *testing.T) {
+	s := ToString("1d23")
+	if s != "1d23" {
+		t.Error("err")
+	}
+	log.Println("success", s)
+}
+
+func TestInt(t *testing.T) {
+	s := ToString(int8(-128))
+	if s != "-128" {
+		t.Error("err")
+	}
+	log.Println("success", s)
+
+	s = ToString(int8(127))
+	if s != "127" {
+		t.Error("err")
+	}
+	log.Println("success", s)
+
+	s = ToString(int16(-32768))
+	if s != "-32768" {
+		log.Println("success", s)
+		t.Error("err")
+	}
+
+	s = ToString(int16(32767))
+	if s != "32767" {
+		t.Error("err")
+	}
+	log.Println("success", s)
+
+	s = ToString(int32(-2147483648))
+	if s != "-2147483648" {
+		t.Error("err")
+	}
+	log.Println("success", s)
+
+	s = ToString(int32(2147483647))
+	if s != "2147483647" {
+		t.Error("err")
+	}
+	log.Println("success", s)
+
+	s = ToString(int64(-9223372036854775808))
+	if s != "-9223372036854775808" {
+		t.Error("err")
+	}
+	log.Println("success", s)
+
+	s = ToString(int64(9223372036854775807))
+	if s != "9223372036854775807" {
+		t.Error("err")
+	}
+	log.Println("success", s)
+
+	s = ToString(int(9223372036854775807))
+	if s != "9223372036854775807" {
+		t.Error("err")
+	}
+	log.Println("success", s)
+
+	s = ToString(int(-9223372036854775808))
+	if s != "-9223372036854775808" {
+		t.Error("err")
+	}
+	log.Println("success", s)
+}
+
+func TestUint(t *testing.T) {
+	s := ToString(uint8(255))
+	if s != "255" {
+		t.Error("err")
+	}
+	log.Println("success", s)
+
+	s = ToString(uint16(65535))
+	if s != "65535" {
+		t.Error("err")
+	}
+	log.Println("success", s)
+
+	s = ToString(uint32(4294967295))
+	if s != "4294967295" {
+		t.Error("err")
+	}
+	log.Println("success", s)
+
+	s = ToString(uint64(18446744073709551615))
+	if s != "18446744073709551615" {
+		t.Error("err")
+	}
+	log.Println("success", s)
+
+	s = ToString(uintptr(18446744073709551615))
+	if s != "18446744073709551615" {
+		t.Error("err")
+	}
+	log.Println("success", s)
+
+	s = ToString(uint(18446744073709551615))
+	if s != "18446744073709551615" {
+		t.Error("err")
+	}
+	log.Println("success", s)
+}
+
+type W struct{}
+
+func (v W) String() string {
+	return "1"
+}
+
+type E string
+type F string
+
+func (v E) String() string {
+	return "2"
+}
+func TestStruct(t *testing.T) {
+	s := ToString(struct{}{})
+	if s != "" {
+		t.Error("err")
+	}
+	log.Println("success", s)
+
+	s = ToString(&W{})
+	if s != "1" {
+		t.Error("err")
+	}
+	log.Println("success", s)
+
+	var e E = "111daw"
+
+	s = ToString(e)
+	if s != "2" {
+		t.Error("err")
+	}
+	log.Println("success", s)
+
+	var f F = "111daw"
+
+	s = ToString(f)
+	if s != "111daw" {
+		t.Error("err")
+	}
+	log.Println("success", s)
+}
+
+func TestPointer(t *testing.T) {
+	var i = 1
+	s := ToString(&i)
+	if s != "1" {
+		t.Error("err")
+	}
+	log.Println("success", s)
+}
+
+type I int
+
+func TestSame(t *testing.T) {
+	var i I = 12
+	s := ToString(&i)
+	if s != "12" {
+		t.Error("err")
+	}
+	log.Println("success", s)
+}
+
+func TestFloat(t *testing.T) {
+	var i float64 = 123123.1241231204
+	s := ToString(&i)
+	if s != "123123.1241231204" {
+		t.Error("err")
+	}
+	log.Println("success", s)
+
+	i = 123123
+	s = ToString(&i)
+	if s != "123123" {
+		t.Error("err")
+	}
+	log.Println("success", s)
+}
+
+func TestSlice(t *testing.T) {
+
+	s := ToString([]string{"1", "2", "3"})
+	if s != "1,2,3" {
+		t.Error("err")
+	}
+	log.Println("success", s)
+
+	s = ToString([]string{"1", "2", "3"}, "-")
+	if s != "1-2-3" {
+		t.Error("err")
+	}
+	log.Println("success", s)
 
 }
